c91b19589e6e14d3ef674b3a4c826b083afbf96b
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2023-03-19 19:14:07 +0700

    Improve axios config

--- configs/axiosInstance.ts
+++ configs/axiosInstance.ts
@@ -1,6 +1,7 @@
-// Note: Không được đặt vào trong thư mục plugins vì đây là thư mục đặc biệt của NuxtJS. Đặt vào cần wrap lại và chỉ sử dụng cho các components, không export sử dụng được trong file.ts.
+// Note: Không được đặt vào trong thư mục plugins vì đây là thư mục dir-structure của NuxtJS. Sử dụng để tạo mới các plugins.
 import axios from 'axios';
 
+const PROTOCAL      = import.meta.env.NUXT_PROTOCAL || 'http'
 const HOST_SERVER   = import.meta.env.NUXT_HOST || 'localhost'
 const PORT_SERVER   = import.meta.env.NUXT_PORT_SERVER || 1111
 const API_PREFIX    = import.meta.env.NUXT_ENV === 'dev' ? '' : '/api'  
@@ -8,12 +9,14 @@ const ACCESS_TOKEN  = process.server ? '' : !!localStorage.getItem('ACCESS_TOKEN
 
 const axiosInstance = axios.create({
 
-    baseURL: `http://${HOST_SERVER}:${PORT_SERVER}` + API_PREFIX,
+    baseURL: `${PROTOCAL}://${HOST_SERVER}:${PORT_SERVER}` + API_PREFIX,
     headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${ACCESS_TOKEN}`
-    }
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+        'Access-Control-Allow-Origin': '*'
+    },
+    timeout: 1000
 })
 
 export default axiosInstance;
