761cacc9af12b177707809c9a45ab94f43250447
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2023-03-19 14:58:43 +0700

    Axios, improve get ACCESS_TOKEN in local

--- configs/axiosInstance.ts
+++ configs/axiosInstance.ts
@@ -1,14 +1,14 @@
-// Note: Không được đặt vào trong thư mục plugins vì đây là thư mục đặc biệt của NuxtJS. Phải wrap lại.
+// Note: Không được đặt vào trong thư mục plugins vì đây là thư mục đặc biệt của NuxtJS. Đặt vào cần wrap lại và chỉ sử dụng cho các components, không export sử dụng được trong file.ts.
 import axios from 'axios';
 
-const HOST          = import.meta.env.NUXT_HOSTNAME || 'localhost'
-const PORT_SERVER   = import.meta.env.NUXT_PORT || 1111
-const PREFIX        = import.meta.env.NUXT_DEV === 'dev' ? '' : '/api'        
-const ACCESS_TOKEN  = ''  //localStorage.getItem('ACCESS_TOKEN') || 
+const HOST_SERVER   = import.meta.env.NUXT_HOST || 'localhost'
+const PORT_SERVER   = import.meta.env.NUXT_PORT_SERVER || 1111
+const API_PREFIX    = import.meta.env.NUXT_ENV === 'dev' ? '' : '/api'  
+const ACCESS_TOKEN  = process.server ? '' : !!localStorage.getItem('ACCESS_TOKEN')
 
 const axiosInstance = axios.create({
 
-    baseURL: `http://${HOST}:${PORT_SERVER}` + PREFIX,
+    baseURL: `http://${HOST_SERVER}:${PORT_SERVER}` + API_PREFIX,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
