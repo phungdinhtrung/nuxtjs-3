f9c5734cee8437ba079d4d6b155b73391c1b2402
Phung Dinh Trung <phungdinhtrung@gfd.com.vn>
2023-07-09 18:34:38 +0700

    Change axios store

--- stores/axios/axiosStore.ts
+++ stores/axios/axiosStore.ts
@@ -2,13 +2,13 @@ import axiosInstance from "@/configs/axiosInstance"
 
 export const useAxiosStore = defineStore('AxiosStore', () => {
 
-	// actions
-	const actGetResult = async() => {
-		return await axiosInstance.get('https://jsonplaceholder.typicode.com/posts/1')
-	}
-	
-	
-	return { 
-		actGetResult
-	}})
+// actions
+const actGetResult = async() => {
+    return await axiosInstance.get('https://jsonplaceholder.typicode.com/posts/1')
+}
+
+
+return { 
+    actGetResult
+}})
 
